f6c947aa48fedadca6267881f212d375e0edc296
Saroar Hossain <limonhossain57@gmail.com>
2022-09-20 21:36:59 +0600

    Added the plan structure

--- php/ziyad_vai/2nd_problem.php
+++ php/ziyad_vai/2nd_problem.php
@@ -1,21 +1,33 @@
 <?php
 
-function str_to_char_array($str){
+function str_to_char_array(string $str): array{
+
+    // str_split the string charecters and make it as an array
     $char_arr = str_split($str);
 
-    print_r($char_arr);
+    // Create a loop to go through each charecter of the string
     $array_with_count = [];
     for($i = 0; $i < count($char_arr); $i++){
+
         if(!array_key_exists($char_arr[$i], $output_arr)){
             $array_with_count[$char_arr[$i]] = 1;
         } else {
             $array_with_count[$char_arr[$i]]++;
         }
+
     }
 
+    // First check if the char if exists in the output array.
+        // - if exists then just update the count of the item in the output array
+        // - If not exits then put the char in the sorted position with count 1
 
+
+    // Finally Return the array
     return $array_with_count;
 }
 
 
 print_r(str_to_char_array('collected'));
+
+
+

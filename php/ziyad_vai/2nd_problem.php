e548c6f0c3976b44af69ba18ab153f90930d1d57
Saroar Hossain <limonhossain57@gmail.com>
2022-09-21 14:52:04 +0600

    Update 2nd_problem.php

--- php/ziyad_vai/2nd_problem.php
+++ php/ziyad_vai/2nd_problem.php
@@ -9,18 +9,46 @@ function str_to_char_array(string $str): array{
     $array_with_count = [];
     for($i = 0; $i < count($char_arr); $i++){
 
-        if(!array_key_exists($char_arr[$i], $output_arr)){
+        // First check if the char if exists in the output array.
+        if(array_key_exists($char_arr[$i], $array_with_count)){
+            // - if exists then just update the count of the item in the output array
+            $array_with_count[$char_arr[$i]]++;
+        } elseif(empty($array_with_count)){
             $array_with_count[$char_arr[$i]] = 1;
         } else {
-            $array_with_count[$char_arr[$i]]++;
+            // - If not exits then put the char in the sorted position with count 1
+            $new_output_array = [];
+            // Go through the output array
+            foreach( $array_with_count as $char => $count ){
+
+                if($char > $char_arr[$i]){
+                    // The newly added item is smaller then the current checking
+                    $matched_item_found = false;
+                    foreach($array_with_count as $char_inner => $count_inner){
+
+                        if( $char_inner === $char ){
+                            $temp_index = $char_inner;
+                            $temp_count = $count_inner;
+
+                            $new_output_array[$char_arr[$i]] = 1;
+                            $new_output_array[$temp_index] = $temp_count;
+                            $matched_item_found = true;
+                            continue;
+                        } elseif($matched_item_found){
+                            $new_output_array[$char_inner] = $count_inner;
+                        }
+
+                    }
+                } 
+
+                $new_output_array[$char] = $count;
+            }
+
+
+            return $new_output_array;
         }
 
     }
-
-    // First check if the char if exists in the output array.
-        // - if exists then just update the count of the item in the output array
-        // - If not exits then put the char in the sorted position with count 1
-
 
     // Finally Return the array
     return $array_with_count;
